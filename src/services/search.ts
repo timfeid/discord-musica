de0b9960614408926f8b30ad12cd182c824345b5
root <root@server.loc>
2020-04-14 02:25:32 +0000

    chore: 25?

--- src/services/search.ts
+++ src/services/search.ts
@@ -7,6 +7,6 @@ export default async function search (query: string, total = 1) {
   return await youtube.searchVideos(query, total)
 }
 
-export default async function playlist (query: string, total = 10) {
+export default async function playlist (query: string, total = 25) {
   return await youtube.getPlaylistItems(query, total)
 }
