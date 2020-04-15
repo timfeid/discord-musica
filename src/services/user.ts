1597f93ea81c5f41aa189f1927130cd2d9fc1ef7
Tim Feid <tim.feid@tellimer.com>
2020-04-14 22:09:30 -0400

    fix: attach em

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,13 +2,18 @@ import {User as UserEntity} from '../data/entities/user'
 import { Guild, User } from 'discord.js'
 import { City } from '../data/entities/city'
 
-const randomCity = async () => {
-  return City.createQueryBuilder().orderBy('random()').getOne() || new City
+const randomCity = async () : Promise<City> => {
+  return (await City.createQueryBuilder().orderBy('random()').getOne()) || new City
 }
 
 const findOrCreate = async (user: User) => {
   const id = parseInt(user.id, 10)
   let u = await UserEntity.findOne(id) || await UserEntity.create({id, currentCity: await randomCity()}).save()
+
+  if (!u.currentCity) {
+    u.currentCity = await randomCity()
+    u.save()
+  }
 
   return u
 }
